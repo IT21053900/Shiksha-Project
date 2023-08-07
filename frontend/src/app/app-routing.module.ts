5228ad7c0b8511817f1c099881af4ceda2b26c64
IT21053900 <it21053900@my.sliit.lk>
2023-08-07 11:45:26 +0530

    register

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentEditComponent } from './components/student-edit/student-edit.component';
 
 const routes: Routes = [
-  {
-    path:'register',component:RegisterComponent
-  }
+  {path:'register',component:RegisterComponent,},
+  {path:'student-list',component:StudentListComponent},
+  {path:'student-edit/:id',component:StudentEditComponent},
 ];
 
 @NgModule({
--- frontend/src/app/students.service.ts
+++ frontend/src/app/students.service.ts
@@ -13,4 +13,21 @@ export class StudentsService {
     return this.http.post('http://localhost:8070/student/add',student);
 
   }
+
+  StudentList(){
+    return this.http.get('http://localhost:8070/student/view');
+  }
+
+  deleteStudent(id:any){
+    return this.http.delete('http://localhost:8070/student/delete/'+id);
+  }
+
+  singleStudent(id:any){
+    return this.http.get('http://localhost:8070/student/get/'+id);
+  }
+
+  updateStudent(id:any,student:any){
+    return this.http.put('http://localhost:8070/student/update/'+id,student);
+  }
+
 }
